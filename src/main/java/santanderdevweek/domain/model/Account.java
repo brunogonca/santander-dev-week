88bcf02f2f293959cb2251bab2b029aeac92c81a
Bruno Gonçalves <brgo1999@gmail.com>
2023-07-24 19:22:34 -0300

    fixed precision and scale that was inverted

--- src/main/java/santanderdevweek/domain/model/Account.java
+++ src/main/java/santanderdevweek/domain/model/Account.java
@@ -12,9 +12,9 @@ public class Account {
     @Column(unique = true)
     private String number;
     private String agency;
-    @Column(nullable = false, scale = 13, precision = 2)
+    @Column(nullable = false, precision = 13, scale = 2)
     private BigDecimal balance;
-    @Column(name = "additional_limit", scale = 13, precision = 2)
+    @Column(nullable = false, precision = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {return id;}
